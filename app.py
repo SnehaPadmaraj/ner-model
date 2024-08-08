0fe2ded3c07222b344ef13094997ebb50929c48b
Sneha Padmaraj <snehapadmaraj22@gmail.com>
2024-08-08 09:10:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import subprocess
-import pandas as pd
-import spacy
-import spacy_streamlit
-import Task
-import Suggestions
-import EmailVisualiser
+import pandas as pd 
 
 st.set_page_config(
     page_title="Home",
@@ -24,23 +18,9 @@ st.markdown(
   """,
     unsafe_allow_html=True
 )
-try:
-    nlp = spacy.load('en_core_web_sm')
-except OSError:
-    st.warning("Downloading spaCy model...")
-    import subprocess
-    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load('en_core_web_sm')
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", ["Home", "Email Visualizer", "Task", "Suggestions"])
 
-if selection == "Home":
-    st.markdown("""
-         ### Click on the sidebar to navigate to the different pages each with their own insights on the Enron Email Dataset
-         """)
-elif selection == "Email Visualizer":
-    EmailVisualiser.email_visualiser()
-elif selection == "Task":
-    Task.task()  
-elif selection == "Suggestions":
-    Suggestions.suggestions() 
+
+st.markdown("""
+         # How to navigate the app:
+         ### Click on the sidebar to navigate to the different pages each with their own insights on the Enron Email Dataset""")
+
